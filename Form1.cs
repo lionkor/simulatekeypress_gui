ab7b9b5aa1c33f422e9305e30b2ae7715e697335
Leed <leed_off@outlook.com>
2018-06-04 19:06:30 +0200

    initial commit, v1.1.0.0

---
+++ Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SimulateKeypress_GUI
+{
+    public partial class Form1 : Form
+    {
+        public bool Stop = false;
+
+        public Form1 ()
+        {
+            InitializeComponent ();
+            Text += ProductVersion;
+        }
+
+        private void StartButton_Click (object sender, EventArgs e)
+        {
+            if (TextBox.Text.Length == 0)
+            {
+                return;
+            }
+            
+            Stop = false;
+            StopButton.Enabled = true;
+            disableAll ();
+            Program.Start (
+                this, Convert.ToInt32 (TimeBeforeStart.Value),
+                Convert.ToInt32 (DelayMS.Value),
+                TextBox.Lines, returnExclamation.Checked,
+                returnPeriod.Checked, returnR.Checked,
+                returnQuestion.Checked, InfiniteLoop.Checked);
+            stop ();
+            
+            enableAll ();
+        }
+
+        private void StopButton_Click (object sender, EventArgs e)
+        {
+            stop ();
+        }
+
+        void stop ()
+        {
+            Stop = true;
+            StopButton.Enabled = false;
+            enableAll ();
+        }
+
+        void disableAll ()
+        {
+            StartButton.Enabled = false;
+            DelayMS.Enabled = false;
+            returnExclamation.Enabled = false;
+            returnPeriod.Enabled = false;
+            returnQuestion.Enabled = false;
+            returnR.Enabled = false;
+            TextBox.Enabled = false;
+            InfiniteLoop.Enabled = false;
+            TimeBeforeStart.Enabled = false;
+        }
+
+        void enableAll ()
+        {
+            StartButton.Enabled = true;
+            DelayMS.Enabled = true;
+            returnExclamation.Enabled = true;
+            returnPeriod.Enabled = true;
+            returnQuestion.Enabled = true;
+            returnR.Enabled = true;
+            TextBox.Enabled = true;
+            InfiniteLoop.Enabled = true;
+            TimeBeforeStart.Enabled = true;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SimulateKeypress_GUI
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main ()
+        {
+            Application.EnableVisualStyles ();
+            Application.SetCompatibleTextRenderingDefault (false);
+            Application.Run (new Form1 ());
+        }
+
+        public static void Start (Form1 form, int startDelay, int charDelay,
+            string[] array, bool returnE, bool returnP, bool returnR, bool returnQ, bool loop)
+        {
+            Thread.Sleep (startDelay);
+            string text = "";
+            foreach (string line in array)
+            {
+                if (returnR)
+                {
+                    text += line + '\r';
+                }
+                else
+                {
+                    text += line;
+                }
+            }
+
+            while (text.Contains ("..") || text.Contains ("!!") || text.Contains ("??"))
+            {
+                text = text.Replace ("..", ".");
+                text = text.Replace ("!!", "!");
+                text = text.Replace ("??", "?");
+            }
+            do
+            {
+                foreach (char c in text)
+                {
+                    Thread.Sleep (charDelay);
+                    if (form.Stop)
+                    {
+                        return;
+                    }
+                    try
+                    {
+                        SendKeys.SendWait (c.ToString ());
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine (c + " : " + e);
+                    }
+                    if (returnP && c == '.')
+                    {
+                        SendKeys.SendWait ("\r");
+                    }
+                    if (returnQ && c == '?')
+                    {
+                        SendKeys.SendWait ("\r");
+                    }
+                    if (returnE && c == '!')
+                    {
+                        SendKeys.SendWait ("\r");
+                    }
+                    //if (returnR && c == '\r')
+                    //{
+                    //    SendKeys.SendWait ("\r");
+                    //}
+                }
+            }
+            while (loop);
+        }
+    }
+}
